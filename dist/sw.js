67dfdc3e1f806218bacc5dad4a30a6cd351ca5cb
ewgenius <ewgeniux@gmail.com>
2016-09-20 12:35:24 +0300

    sw init

---
+++ dist/sw.js
@@ -0,0 +1,19 @@
+const staticCacheName = 'periodic-papers-static-v2' 
+
+self.addEventListener('install', event => {
+  console.log('installed')
+})
+
+self.addEventListener('activate', event => {
+  console.log('activated')  
+})
+
+self.addEventListener('fetch', event => {
+
+})
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting()
+  }
+})
--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,53 @@ import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles'
 
 const container = document.querySelector('#root')
 
+// init sw
+
+function updateReady(worker) {
+  console.log('update is ready')
+  /*store.dispatch(showAlert('New version is ready', 'reload', () => {
+    worker.postMessage({ action: 'skipWaiting' })
+  }))*/
+}
+
+function trackInstalling(worker) {
+  console.log('installing')
+  worker.addEventListener('statechange', () => {
+    updateReady(worker)
+  })
+}
+
+if ('serviceWorker' in navigator) {
+  const serviceWorker = navigator['serviceWorker']
+
+  serviceWorker.register('/sw.js')
+    .then(reg => {
+      if (!serviceWorker.controller) {
+        return
+      }
+
+      if (reg.waiting) {
+        updateReady(reg.waiting)
+        return
+      }
+
+      if (reg.installing) {
+        trackInstalling(reg.installing)
+        return
+      }
+
+      reg.addEventListener('updatefound', () => {
+        trackInstalling(reg.installing)
+      })
+    })
+
+  let refreshing = false
+  serviceWorker.addEventListener('controllerchange', () => {
+    if (refreshing) return
+    window.location.reload()
+    refreshing = true
+  })
+}
 
 // init material ui
 
