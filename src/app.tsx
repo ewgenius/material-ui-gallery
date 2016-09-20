10921f059864551369f09f8d7be188359f63bb1b
ewgenius <ewgeniux@gmail.com>
2016-09-20 12:28:06 +0300

    upd

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,20 @@
+import '../styles/main.scss'
 import * as React from 'react'
 import {render} from 'react-dom'
 import * as configureTapEvent from 'react-tap-event-plugin'
+
+import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles'
 
 configureTapEvent()
 
 const container = document.querySelector('#root')
 
-render(<div>app</div>, container)
+const theme = getMuiTheme({
+  appBar: {
+    height: 56
+  }
+})
+
+render(<MuiThemeProvider muiTheme={theme}>
+  <div>app</div>
+</MuiThemeProvider>, container)
